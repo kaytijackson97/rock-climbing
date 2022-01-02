5715c5043b959a306b5b98d059c49f891f1d129f
kaytiwitia <kaytiwiita@github.com>
2022-01-02 00:03:24 -0500

    updating welcome page with link to climbers list

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+
+import { CLIENT_ENDPOINTS } from '../constants/Routes';
+
 function Welcome() {
+    const { CLIMBERS } = CLIENT_ENDPOINTS;
+
     return (
         <div>
             <h1>Rock Climbing</h1>
+            <button type="button" className="btn btn-primary">
+                <Link to={`${CLIMBERS}`} className="text-white text-decoration-none">
+                    Select a Climber
+                </Link>
+            </button>
         </div>
     );
 };
